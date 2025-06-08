13d0001a10693c2df9f42df171aaebb919ecee7b
Sajal Sabat <sajalsabat@gmail.com>
2025-06-08 19:36:37 -0400

    added response former

---
+++ scripts/gemini_prompt.py
@@ -0,0 +1,21 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gemini_apikey = os.getenv("GEMINI_APIKEY")
+
+
+def get_response(chunks, query):
+    client = genai.Client(api_key=gemini_apikey)
+
+    context = "\n\n".join(chunks)
+    prompt = f"""Use the following context to answer the question (only give the answer, no formatting).
+                Context: {context}
+                Question: {query}
+                Answer:"""
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=query
+    )
+    return response.text
